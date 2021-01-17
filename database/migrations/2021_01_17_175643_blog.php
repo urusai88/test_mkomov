418681f0c8da81ee525187cf4713110bb3f32419
urusai88 <archon.saratov@gmail.com>
2021-01-17 22:01:57 +0400

    Миграция статьи, комметарии

---
+++ database/migrations/2021_01_17_175643_blog.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Blog extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $blueprint) {
+            $blueprint->increments('id')->primary();
+            $blueprint->text('body');
+            $blueprint->integer('likes_count')->default(0);
+            $blueprint->integer('views_count')->default(0);
+        });
+
+        Schema::create('comments', function (Blueprint $blueprint) {
+            $blueprint->increments('id')->primary();
+            $blueprint->text('subject');
+            $blueprint->text('body');
+            $blueprint->integer('article_id');
+
+            $blueprint->foreign('article_id')
+                ->references('id')->on('articles')
+                ->cascadeOnDelete()
+                ->cascadeOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('comments');
+        Schema::drop('articles');
+    }
+}
