5bb9c15fca9738ec400a16dc69f64e639afc81fc
urusai88 <archon.saratov@gmail.com>
2021-01-22 19:16:12 +0400

    clear

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:beamer/beamer.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:url_strategy/url_strategy.dart';
@@ -31,40 +30,6 @@ class MyApp extends StatelessWidget {
       routeInformationParser: routeParser,
       routerDelegate: routerDelegate,
     );
-
-    /*
-    Widget app = MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      onGenerateRoute: (settings) {
-        Widget? parseRoute(String name, Object? arguments) {
-          if (name == '/articles') {
-            return ArticleCatalogScreen();
-          }
-
-          var match = RegExp(r'^/articles/([\w-]+).([\d]+)').firstMatch(name);
-          if (match != null) {
-            final slug = match.group(1);
-            final id = int.parse(match.group(2)!);
-
-            return ArticleScreen(id: id);
-          }
-        }
-
-        Widget? widget;
-
-        if (settings.name != null) {
-          widget = parseRoute(settings.name!, settings.arguments);
-        }
-
-        return MaterialPageRoute(
-          builder: (_) => widget ?? MainScreen(),
-          settings: settings,
-        );
-      },
-    );*/
 
     app = MultiProvider(
       providers: [
